06fdd7b6805bba23d5f753ec820f83c32b8f6e1a
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 11:13:17 +0700

    Extract method & rename var

--- ch1/statement.js
+++ ch1/statement.js
@@ -11,10 +11,7 @@ function statement (invoice, plays) {
                           minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
 
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     //print line for this order
     result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
@@ -22,6 +19,13 @@ function statement (invoice, plays) {
   }
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
   return result;
 }
 
