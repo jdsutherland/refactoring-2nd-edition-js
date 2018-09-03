61b5e9027e2e05e9a786e21e0e22b522bcece33d
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 15:30:54 +0700

    Begin replace conditional polymorphism
    Create a PerformanceCalculator to contain inheritance hierarchy

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+}
+
 function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
@@ -7,8 +14,9 @@ function createStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
