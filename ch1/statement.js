42d562c47b5f4be10635a60e87085f418ee6aece
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 14:43:34 +0700

    Split Phase extract to separate module

--- ch1/statement.js
+++ ch1/statement.js
@@ -3,68 +3,13 @@
 // use `require` as workaround
 const invoice = require('./invoices')[0];
 const plays = require('./plays');
+const createStatementData = require('./createStatementData');
 
-function statement (invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText (statementData, plays);
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function totalAmount(data) {
-    return data.performances
-      .reduce((total, p) => total + p.amount, 0)
-  }
-
-  function totalVolumeCredits(data) {
-    return data.performances
-      .reduce((total, p) => total + p.volumeCredits, 0)
-  }
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
-function renderPlainText (data, plays) {
+function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (const perf of data.performances) {
     result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
