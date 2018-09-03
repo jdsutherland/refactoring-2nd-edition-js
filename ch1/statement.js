35559ea83791b9c58d9452f43e1bc61ac2afcf03
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 10:03:16 +0700

    Refactor extract function

--- ch1/statement.js
+++ ch1/statement.js
@@ -7,25 +7,7 @@ function statement (invoice, plays) {
                           minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play)
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -41,7 +23,26 @@ function statement (invoice, plays) {
   return result;
 }
 
-const invoice = require('./invoices');
-const plays = require('./plays');
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+  case "tragedy":
+    thisAmount = 40000;
+    if (perf.audience > 30) {
+      thisAmount += 1000 * (perf.audience - 30);
+    }
+    break;
+  case "comedy":
+    thisAmount = 30000;
+    if (perf.audience > 20) {
+      thisAmount += 10000 + 500 * (perf.audience - 20);
+    }
+    thisAmount += 300 * perf.audience;
+    break;
+  default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+}
 
 module.exports = statement;
