550cf3bb2978ae1de05c6cd9321dbb0a1cf064d2
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 15:43:51 +0700

    Move volumeCredits

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -25,6 +25,13 @@ class PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
@@ -40,19 +47,12 @@ function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 
   function totalAmount(data) {
