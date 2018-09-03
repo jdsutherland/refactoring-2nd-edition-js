b2f99eb9f07344c70563286824f29d293f307f31
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 15:56:16 +0700

    Complete Replace Conditional W/ Polymorphism
    Make Play types polymorphic for amount calculation using inheritance

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -5,25 +5,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (this.performance.audience > 30) {
-        result += 1000 * (this.performance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (this.performance.audience > 20) {
-        result += 10000 + 500 * (this.performance.audience - 20);
-      }
-      result += 300 * this.performance.audience;
-      break;
-    default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
+    throw new Error('subclass responsiblity');
   }
 
   get volumeCredits() {
@@ -31,6 +13,26 @@ class PerformanceCalculator {
     result += Math.max(this.performance.audience - 30, 0);
     if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
     return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    return result += 300 * this.performance.audience;
   }
 }
 
@@ -43,12 +45,22 @@ function createStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
     result.volumeCredits = calculator.volumeCredits;
     return result;
+  }
+
+  function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+      case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+      case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+      default:
+        throw new Error(`unknown type: ${aPlay.type}`);
+
+    }
   }
 
   function playFor(aPerformance) {
