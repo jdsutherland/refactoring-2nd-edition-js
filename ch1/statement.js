d68a0f2ce401b43a89b3d2050767ddc219cb95c4
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 10:56:41 +0700

    Change function declaration

--- ch1/statement.js
+++ ch1/statement.js
@@ -10,7 +10,7 @@ function statement (invoice, plays) {
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf))
+    let thisAmount = amountFor(perf)
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -30,9 +30,9 @@ function playFor(aPerforamance) {
   return plays[aPerforamance.playID];
 }
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
   let result = 0;
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
   case "tragedy":
     result = 40000;
     if (aPerformance.audience > 30) {
@@ -47,7 +47,7 @@ function amountFor(aPerformance, play) {
     result += 300 * aPerformance.audience;
     break;
   default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
   }
   return result;
 }
