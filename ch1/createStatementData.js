e9a1e2fbad8e0237374607f345f1f7a62fadf556
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-03 16:02:00 +0700

    Use polymorphism for volumeCredits

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -9,10 +9,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);;
   }
 }
 
@@ -33,6 +30,10 @@ class ComedyCalculator extends PerformanceCalculator {
       result += 10000 + 500 * (this.performance.audience - 20);
     }
     return result += 300 * this.performance.audience;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
