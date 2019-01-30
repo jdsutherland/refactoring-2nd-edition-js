3ca9bba8eef9c783fc545481bcd96fc677446eaa
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-30 15:57:27 +0700

    Add initial extract class

---
+++ extract_class.js
@@ -0,0 +1,39 @@
+reading = {customer : "ivan", quantity : 10, month : 5, year : 2017};
+
+const aReading = aquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+// then
+const aReading = acquireReading();
+const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+// then
+const aReading = acquireReading();
+const basicChargeAmount = calculateBaseCharge(aReading);
+
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+// then
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() { return this._customer; }
+  get quantity() { return this._quantity; }
+  get month() { return this._month; }
+  get year() { return this._year; }
+
+  get baseCharge() {
+    return baseRate(this.month, this.year) * this.quantity;
+  }
+}
+
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const basicChargeAmount = aReading.baseCharge;
