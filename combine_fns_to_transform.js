a0cabf3eb63e04ace6aa0b4d6ed361a6d8759fac
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-30 16:15:20 +0700

    Add enrichment fn

--- combine_fns_to_transform.js
+++ combine_fns_to_transform.js
@@ -4,3 +4,16 @@ const basicChargeAmount = calculateBaseCharge(aReading);
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.year) * aReading.quantity;
 }
+
+// then
+
+function enrichReading(original) {
+  // lodash cloneDeep
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  return result;
+}
+
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const basicChargeAmount = aReading.baseCharge
