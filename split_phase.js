6d1ae1e76c8f15d76793e76fee8b8da393e9fdba
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-30 16:27:53 +0700

    Extract a fn

--- split_phase.js
+++ split_phase.js
@@ -9,3 +9,21 @@ function priceOrder(product, quantity, shippingMethod) {
   const price = basePrice - discount + shippingCost;
   return price;
 }
+
+// then
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold, 0) *
+                   product.basePrice * product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+}
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+  const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+                              ? shippingMethod.discountedFee
+                              : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+}
