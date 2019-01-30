383896195eb94790e354b54bfd990e602a7f7010
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-30 16:13:47 +0700

    Initial transform

---
+++ combine_fns_to_transform.js
@@ -0,0 +1,6 @@
+const aReading = acquireReading();
+const basicChargeAmount = calculateBaseCharge(aReading);
+
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
