a4b0a9c60b69f6404593a45e57e7862670241fd8
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-30 16:32:31 +0700

    Add intermediate data structure to communicate between

--- split_phase.js
+++ split_phase.js
@@ -16,14 +16,15 @@ function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0) *
                    product.basePrice * product.discountRate;
-  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  const priceData = {basePrice: basePrice quantity: quantity, discount: discount};
+  const price = applyShipping(priceData, shippingMethod);
   return price;
 }
 
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
-  const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
                               ? shippingMethod.discountedFee
                               : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
 }
