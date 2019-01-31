f9171fb0a5c8199bffcfd1fb10300cb538a1f14a
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:26:09 +0700

    Add initial

---
+++ ch07_encapsulation/replace_temp_with_query.js
@@ -0,0 +1,13 @@
+class Order {
+  constructor(quantity, item) {
+    this._quantity = quantity;
+    this._item = item;
+  }
+  get price() {
+    var basePrice = this._quantity * this._item.price;
+    var discountFactor = 0.98;
+    if (basePrice > 1000)
+      discountFactor ­ = 0.03;
+    return basePrice * discountFactor;
+  }
+}
