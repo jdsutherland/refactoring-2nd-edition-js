05dee9d54212c1329635ca0bc1b9b9a440f93656
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 15:21:08 +0700

    Finish moving to subclass

--- ch10_conditionals/using_polymorphism_for_variation.js
+++ ch10_conditionals/using_polymorphism_for_variation.js
@@ -43,16 +43,8 @@ class Rating {
 
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    if (this.voyage.zone === "china" && this.hasChinaHistory) {
-      result += 3;
-      if (this.history.length > 10) result += 1;
-      if (this.voyage.length > 12) result += 1;
-      if (this.voyage.length > 18) result -= 1;
-    }
-    else {
-      if (this.history.length > 8) result += 1;
-      if (this.voyage.length > 14) result -= 1;
-    }
+    if (this.history.length > 8) result += 1;
+    if (this.voyage.length > 14) result -= 1;
     return result;
   }
 
@@ -65,6 +57,15 @@ class ExperiencedChinaRating extends Rating {
   get captainHistoryRisk() {
     const result = super.captainHistoryRisk - 2;
     return Math.max(result, 0);
+  }
+
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    result += 3;
+    if (this.history.length > 10) result += 1;
+    if (this.voyage.length > 12) result += 1;
+    if (this.voyage.length > 18) result -= 1;
+    return result;
   }
 }
 
