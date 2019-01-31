b3b1eed3e158ebc822492fa04bb12723c297af1a
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 15:58:45 +0700

    Add the assertion

--- ch10_conditionals/add_assertion.js
+++ ch10_conditionals/add_assertion.js
@@ -1,7 +1,9 @@
 class Customer {
   applyDiscount(aNumber) {
-    return (this.discountRate)
-      ? aNumber - (this.discountRate * aNumber)
-      : aNumber;
+    if (!this.discountRate) return aNumber;
+    else {
+      assert(this.discountRate >= 0);
+      return aNumber - (this.discountRate * aNumber);
+    }
   }
 }
