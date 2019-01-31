e58cb3826d86b23a263f7e38392df6a734fa23ba
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 09:14:30 +0700

    Encaps var

--- ch07_encapsulation/nested_record_to_class.js
+++ ch07_encapsulation/nested_record_to_class.js
@@ -27,3 +27,16 @@ function compareUsage (customerID, laterYear, month) {
   const earlier = customerData[customerID].usages[laterYear - 1][month];
   return {laterAmount: later, change: later - earlier};
 }
+
+// 1. encapsulate var
+
+function getRawDataOfCustomers() { return customerData; }
+function setRawDataOfCustomers(arg) { return customerData = arg; }
+
+getRawDataOfCustomers()[customerID].usages[year][month] = amount
+
+function compareUsage (customerID, laterYear, month) {
+  const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+  const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
+  return {laterAmount: later, change: later - earlier};
+}
