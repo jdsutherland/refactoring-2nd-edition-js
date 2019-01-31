244f0e0fe6abcc3700e6b49ac9f967632b6ba806
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:46:54 +0700

    Finish but TODO Change Ref To Value

--- ch07_encapsulation/extract_class.js
+++ ch07_encapsulation/extract_class.js
@@ -2,18 +2,21 @@ class Person {
   constructor() {
     this._telephoneNumber = new TelephoneNumber();
   }
-
   get name() { return this._name; }
   set name(arg) { this._name = arg; }
-  get telephoneNumber() { return this._telephoneNumber.telephoneNumber; }
+
+  get telephoneNumber() { return this._telephoneNumber.toString(); }
   get officeAreaCode() { return this._telephoneNumber.areaCode; }
   set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
   get officeNumber() { return this._telephoneNumber.number; }
   set officeNumber(arg) { this._telephoneNumber.number = arg; }
 }
 
+// TODO: replace "office" methods with accessors for telephone #
+// this way TelephoneNumber will act better as a Value Object, so apply refactoring: 'Change Ref to Value'
+
 class TelephoneNumber {
-  get telephoneNumber() {
+  toString() {
     return `(${this.areaCode}) ${this.number}`;
   }
 
@@ -23,3 +26,4 @@ class TelephoneNumber {
   get number() { return this._number; }
   set number(arg) { this._number = arg; }
 }
+
