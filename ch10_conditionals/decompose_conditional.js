4d8afd0dc1cb95a282773374ad00b411da6034ca
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:40:24 +0700

    Add init

---
+++ ch10_conditionals/decompose_conditional.js
@@ -0,0 +1,4 @@
+if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+  charge = quantity * plan.summerRate;
+else
+  charge = quantity * plan.regularRate + plan.regularServiceCharge;
