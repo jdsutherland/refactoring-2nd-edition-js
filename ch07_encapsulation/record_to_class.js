a23e76b3188e57733d80a5aa754ca12d55d1d400
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 08:57:33 +0700

    Step 2 add class

--- ch07_encapsulation/record_to_class.js
+++ ch07_encapsulation/record_to_class.js
@@ -4,8 +4,20 @@ const organization = {name: "Acme Gooseberries", country: "GB"};
 result += `<h1>${organization.name}</h1>`;
 organization.name = newName;
 
-// 1st step: encap. var
+// 1. encap. var
 function getRawOrganization() { return organization; }
 
 result += `<h1>${getRawOrganization().name}</h1>`;
 getRawOrganization().name = newName;
+
+// 2. create class
+
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+const organization = new Organization({name: "Acme Gooseberries", country: "GB"})
+function getRawOrganization() { return organization._data; }
+function getOrganization() { return organization; }
