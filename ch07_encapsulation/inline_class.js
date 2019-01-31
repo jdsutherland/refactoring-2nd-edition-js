75f47bdef56585afd33f72d6f254635c37f2294a
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:49:13 +0700

    Add initial

---
+++ ch07_encapsulation/inline_class.js
@@ -0,0 +1,23 @@
+class TrackingInformation {
+  get shippingCompany() { return this._shippingCompany; }
+  set shippingCompany(arg) { this._shippingCompany = arg; }
+  get trackingNumber() { return this._trackingNumber; }
+  set trackingNumber(arg) { this._trackingNumber = arg; }
+  get display() {
+    return `${this.shippingCompany} : $ { this.trackingNumber }
+    `;
+  }
+}
+
+// TrackingInfo seems better inlined into Shipment
+
+class Shipment {
+  get trackingInfo() { return this._trackingInformation.display; }
+  get trackingInformation() { return this._trackingInformation; }
+  set trackingInformation(aTrackingInformation) {
+    this._trackingInformation = aTrackingInformation;
+  }
+}
+
+// ex client
+aShipment.trackingInformation.shippingCompany = request.vendor;
