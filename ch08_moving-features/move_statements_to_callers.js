3831e5f8b45a5a88efebf1b511f5b49cb3e14052
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:29:24 +0700

    Rename to orig

--- ch08_moving-features/move_statements_to_callers.js
+++ ch08_moving-features/move_statements_to_callers.js
@@ -1,7 +1,7 @@
 function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
-  zztmp(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
   outStream.write(`<p>location: ${person.photo.location}</p>\n`);
 }
 
@@ -10,17 +10,16 @@ function listRecentPhotos(outStream, photos) {
     .filter(p => p.date > recentDateCutoff())
     .forEach(p => {
       outStream.write("<div>\n");
-      zztmp(outStream, p);
+      emitPhotoData(outStream, p);
       outStream.write(`<p>location: ${p.location}</p>\n`);
       outStream.write("</div>\n");
     });
 }
 
-
 // I need to modify the software so that listRecentPhotos renders the location information differently while renderPerson stays the same.
 // To make this change easier, I’ll use Move Statements to Callers on the final line.
 
-function zztmp(outStream, photo) {
+function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
 }
