5a87935efc30a10c046561ea98e556f58e8c36bc
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 09:13:32 +0700

    Encap nested record initial

---
+++ ch07_encapsulation/nested_record_to_class.js
@@ -0,0 +1,29 @@
+"1920" : {
+  name: "martin",
+  id: "1920",
+  usages: {
+    "2016" : {
+      "1" : 50,
+      "2" : 55,
+      // remaining months of the year
+      },
+    "2015" : {
+      "1" : 70,
+      "2" : 63,
+      // remaining months of the year
+    }
+  }
+},
+"38673": {
+  name: "neal",
+  id : "38673",
+  // ...
+}
+
+customerData[customerID].usages[year][month] = amount
+
+function compareUsage (customerID, laterYear, month) {
+  const later = customerData[customerID].usages[laterYear][month];
+  const earlier = customerData[customerID].usages[laterYear - 1][month];
+  return {laterAmount: later, change: later - earlier};
+}
