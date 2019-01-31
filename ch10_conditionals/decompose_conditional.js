937140ac6a0e6d60a882b2fd2aa8c2a2ab386bad
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:41:09 +0700

    (more)

--- ch10_conditionals/decompose_conditional.js
+++ ch10_conditionals/decompose_conditional.js
@@ -1,8 +1,11 @@
 if (summer())
-  charge = quantity * plan.summerRate;
+  charge = summerCharge();
 else
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
 
 function summer() {
   return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+function summerCharge() {
+  return quantity * plan.summerRate;
 }
