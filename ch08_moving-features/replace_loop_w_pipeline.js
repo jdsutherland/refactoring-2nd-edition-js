8451767fc241e102e4a95b753fdcfb5358200ddd
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:54:28 +0700

    Clean formatting

--- ch08_moving-features/replace_loop_w_pipeline.js
+++ ch08_moving-features/replace_loop_w_pipeline.js
@@ -9,14 +9,12 @@
 
 function acquireData(input) {
   const lines = input.split("\n");
-  const result = [];
-  const loopItems = lines
-    .slice(1)
-    .filter(line => line.trim() !== "")
-    .map(line => line.split(","))
-    .filter(record => record[1].trim() === "India")
-    .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
-  return result;
+  return lines
+    .slice (1)
+    .filter(line   => line.trim() !== "")
+    .map   (line   => line.split(","))
+    .filter(fields => fields[1].trim() === "India")
+    .map   (fields => ({city: fields[0].trim(), phone: fields[2].trim()}))
 }
 
 // I want to replace that loop with a collection pipeline.
