5734c81fb5e027bd5a47e4920fc766242df810ed
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:35:35 +0700

    Move to sep fn

--- ch08_moving-features/split_loop.js
+++ ch08_moving-features/split_loop.js
@@ -1,13 +1,18 @@
 function foo() {
-  let youngest = people[0] ? people[0].age : Infinity;
   let totalSalary = 0;
   for (const p of people) {
     totalSalary += p.salary;
   }
 
+  return `totalSalary: ${totalSalary}`;
+}
+
+function bar() {
+  let youngest = people[0] ? people[0].age : Infinity;
+
   for (const p of people) {
     if (p.age < youngest) youngest = p.age;
   }
 
-  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+  return `youngestAge: ${youngest}`;
 }
