a90a9ddce383b2c86b99a72ad677cfdb5ccaae17
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:10:46 +0700

    Add initial object

--- ch07_encapsulation/replace_primitive_with_object.js
+++ ch07_encapsulation/replace_primitive_with_object.js
@@ -3,11 +3,18 @@ class Order {
     this.priority = data.priority;
     // more instantiation
   }
-  get priority() { return this.priority; }
-  set priority(aString) { this.priority = aString; }
+  get priority() { return this.priority.toString(); }
+  set priority(aString) { this.priority = new Priority(aString); }
 }
 
 // ex client code
 highPriorityCount =
     orders.filter(o = > "high" == = o.priority || "rush" == = o.priority)
         .length;
+
+class Priority {
+  constructor(value) {
+    this._value = value;
+  }
+  toString() { return this_.value; }
+}
