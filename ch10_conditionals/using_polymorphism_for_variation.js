e88048528b737791213d308a81d150d81c68c543
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 15:27:27 +0700

    Rename now that And is gone

--- ch10_conditionals/using_polymorphism_for_variation.js
+++ ch10_conditionals/using_polymorphism_for_variation.js
@@ -38,11 +38,11 @@ class Rating {
     if (this.voyage.zone === "china") result += 1;
     if (this.voyage.zone === "east-indies") result += 1;
     result += this.historyLengthFactor;
-    result += voyageAndHistoryLengthFactor;
+    result += this.voyageLengthFactor;
     return result;
   }
 
-  get voyageAndHistoryLengthFactor() {
+  get voyageLengthFactor() {
     let result = 0;
     if (this.voyage.length > 14) result -= 1;
     return result;
@@ -63,7 +63,7 @@ class ExperiencedChinaRating extends Rating {
     return Math.max(result, 0);
   }
 
-  get voyageAndHistoryLengthFactor() {
+  get voyageLengthFactor() {
     let result = 0;
     result += 3;
     if (this.voyage.length > 12) result += 1;
