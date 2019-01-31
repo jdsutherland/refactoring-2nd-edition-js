aa1d195f92618fb93850beac2ace75c34b2df513
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:34:25 +0700

    Split the loops

---
+++ ch08_moving-features/split_loop.js
@@ -0,0 +1,13 @@
+function foo() {
+  let youngest = people[0] ? people[0].age : Infinity;
+  let totalSalary = 0;
+  for (const p of people) {
+    totalSalary += p.salary;
+  }
+
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.age;
+  }
+
+  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+}
