490d58e5d4b2a57605544c5e092ff8fad254977f
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:37:45 +0700

    Add init

---
+++ ch08_moving-features/replace_loop_w_pipeline.js
@@ -0,0 +1,28 @@
+// Imagine we have the following CSV data:
+
+// office, country, telephone
+// Chicago, USA, +1 312 373 1000
+// Beijing, China, +86 4008 900 505
+// Bangalore, India, +91 80 4064 9570
+// Porto Alegre, Brazil, +55 51 3079 3550
+// Chennai, India, +91 44 660 44766
+
+function acquireData(input) {
+  const lines = input.split("\n");
+  let firstLine = true;
+  const result = [];
+  for (const line of lines) {
+    if (firstLine) {
+      firstLine = false;
+      continue;
+    }
+    if (line.trim() === "") continue;
+    const record = line.split(",");
+    if (record[1].trim() === "India") {
+      result.push({city: record[0].trim(), phone: record[2].trim()});
+    }
+  }
+  return result;
+}
+
+// I want to replace that loop with a collection pipeline.
