78bb4ed05bc491e5a76250719e996f033c54ca53
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:26:53 +0700

    Add init

---
+++ ch08_moving-features/move_statements_to_callers.js
@@ -0,0 +1,24 @@
+function renderPerson(outStream, person) {
+  outStream.write(`<p>${person.name}</p>\n`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
+}
+
+function listRecentPhotos(outStream, photos) {
+  photos
+    .filter(p => p.date > recentDateCutoff())
+    .forEach(p => {
+      outStream.write("<div>\n");
+      emitPhotoData(outStream, p);
+      outStream.write("</div>\n");
+    });
+}
+
+function emitPhotoData(outStream, photo) {
+  outStream.write(`<p>title: ${photo.title}</p>\n`);
+  outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
+  outStream.write(`<p>location: ${photo.location}</p>\n`);
+}
+
+// I need to modify the software so that listRecentPhotos renders the location information differently while renderPerson stays the same.
+// To make this change easier, I’ll use Move Statements to Callers on the final line.
