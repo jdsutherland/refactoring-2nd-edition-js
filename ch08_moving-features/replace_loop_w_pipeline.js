94c69685fc06cf71d84d66b963e1c311eb1c8847
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:47:26 +0700

    Map

--- ch08_moving-features/replace_loop_w_pipeline.js
+++ ch08_moving-features/replace_loop_w_pipeline.js
@@ -15,9 +15,7 @@ function acquireData(input) {
     .filter(line => line.trim() !== "")
     .map(line => line.split(","))
     .filter(record => record[1].trim() === "India")
-  for (const line of loopItems) {
-    result.push({city: record[0].trim(), phone: record[2].trim()});
-  }
+    .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
   return result;
 }
 
