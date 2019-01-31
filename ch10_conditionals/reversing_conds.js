204f44cf7d03da618d842f7f8fc158f7745e380b
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:57:08 +0700

    Bonus strawberry

--- ch10_conditionals/reversing_conds.js
+++ ch10_conditionals/reversing_conds.js
@@ -1,8 +1,6 @@
 function adjustedCapital(anInstrument) {
-  let result = 0;
   if ( anInstrument.capital      <= 0
     || anInstrument.interestRate <= 0
-    || anInstrument.duration     <= 0) return result;
-  result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-  return result;
+    || anInstrument.duration     <= 0) return 0;
+  return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
