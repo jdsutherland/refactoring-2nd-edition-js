0007560de5a98a8efb894eb2221c452f931d7b11
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:44:02 +0700

    again

--- ch10_conditionals/consolidate_cond_expr.js
+++ ch10_conditionals/consolidate_cond_expr.js
@@ -1,5 +1,5 @@
 function disabilityAmount(anEmployee) {
   if ((anEmployee.seniority < 2)
-      || (anEmployee.monthsDisabled > 12)) return 0;
-  if (anEmployee.isPartTime) return 0;
+      || (anEmployee.monthsDisabled > 12)
+      || (anEmployee.isPartTime)) return 0;
   // compute the disability amount
