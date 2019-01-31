3cf5e57335499c16062b2f367be84ee7c5789c8c
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:41:18 +0700

    Replace blank check w filter

--- ch08_moving-features/replace_loop_w_pipeline.js
+++ ch08_moving-features/replace_loop_w_pipeline.js
@@ -11,9 +11,9 @@ function acquireData(input) {
   const lines = input.split("\n");
   const result = [];
   const loopItems = lines
-    .slice(1);
+    .slice(1)
+    .filter(line => line.trim() !== "")
   for (const line of loopItems) {
-    if (line.trim() === "") continue;
     const record = line.split(",");
     if (record[1].trim() === "India") {
       result.push({city: record[0].trim(), phone: record[2].trim()});
