9e7e969960c5f979cfa229121534d749548ac8b4
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 09:03:24 +0700

    Complete encap record to class

--- ch07_encapsulation/record_to_class.js
+++ ch07_encapsulation/record_to_class.js
@@ -14,10 +14,19 @@ getRawOrganization().name = newName;
 
 class Organization {
   constructor(data) {
-    this._data = data;
+    this._name = data.name;
+    this._country = data.country;
   }
+
+  get name() { return this.name }
+  set name(aString) { this._name = aString }
+
+  get country() { return this.country }
+  set country(aCountryCode) { this._country = aCountryCode }
 }
 
 const organization = new Organization({name: "Acme Gooseberries", country: "GB"})
-function getRawOrganization() { return organization._data; }
 function getOrganization() { return organization; }
+
+getOrganization().name = newName;
+result += `<h1>${getOrganization().name}</h1>`;
