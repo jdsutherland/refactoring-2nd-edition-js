7b4f776808d8023b2297a259ee206ae7b8e87982
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 09:54:36 +0700

    Add problematic collection encaps

--- ch07_encapsulation/enscaps_collection.js
+++ ch07_encapsulation/enscaps_collection.js
@@ -5,7 +5,7 @@ class Person {
   }
   get name() { return this._name; }
   get courses() { return this._courses; }
-  set courses(aList) {  this._courses = aList; }
+  set courses(aList) { this._courses = aList; }
 }
 
 class Course {
@@ -17,6 +17,16 @@ class Course {
   get isAdvanced() { return this._isAdvanced; }
 }
 
-numAdvancedCourses = aPerson.courses
-  .filter(c => c.isAdvanced)
-  .length;
+numAdvancedCourses = aPerson.courses.filter(c = > c.isAdvanced).length;
+
+// Person._courses is not properly encapsulated
+
+// is ok
+const basicCourseNames = readBasicCourseNames(filename);
+aPerson.courses = basicCourseNames.map(name = > new Course(name, false));
+
+// but this is problematic b/c Person has no ability to ctrl when the list is updated in this way
+// -- the ref is encaps but the content of the field is not
+for (const name of readBasicCourseNames(filename)) {
+  aPerson.courses.push(new Course(name, false));
+}
