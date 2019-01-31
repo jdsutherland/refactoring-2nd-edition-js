744aaed329395243b44023d0f63afbb3b13b1eed
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:59:54 +0700

    Initial

---
+++ ch07_encapsulation/hide_delegate.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(name) { this._name = name; }
+  get name() { return this._name; }
+  get department() { return this._department; }
+  set department(arg) { this._department = arg; }
+}
+
+class Department {
+  get chargeCode() { return this._chargeCode; }
+  set chargeCode(arg) { this._chargeCode = arg; }
+  get manager() { return this._manager; }
+  set manager(arg) { this._manager = arg; }
+}
+
+// client wants to know manager of a person => needs extra knowledge of department (bad)
+manager = aPerson.department.manager;
