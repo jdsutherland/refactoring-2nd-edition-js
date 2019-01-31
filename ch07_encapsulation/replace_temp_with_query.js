0545e44f9a544390358aef275eb0be75bc2f27eb
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:27:25 +0700

    Extract method

--- ch07_encapsulation/replace_temp_with_query.js
+++ ch07_encapsulation/replace_temp_with_query.js
@@ -4,10 +4,14 @@ class Order {
     this._item = item;
   }
   get price() {
-    const basePrice = this._quantity * this._item.price;
+    const basePrice = this.basePrice;
     var discountFactor = 0.98;
     if (basePrice > 1000)
       discountFactor ­ = 0.03;
     return basePrice * discountFactor;
+  }
+
+  get basePrice() {
+    return this._quantity * this._item.price;
   }
 }
