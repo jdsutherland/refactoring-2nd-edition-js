846c811ce9fd35f5226a0ea2250165ddc585426c
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 15:12:20 +0700

    Combine fn to class (base case)

--- ch10_conditionals/using_polymorphism_for_variation.js
+++ ch10_conditionals/using_polymorphism_for_variation.js
@@ -4,44 +4,55 @@
 // The rating agency gives out either an “A” or “B” rating, depending of various factors due to risk and profit potential. The risk comes from assessing the nature of the voyage as well as the history of the captain’s prior voyages.
 
 function rating(voyage, history) {
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > (vr + chr * 2)) return "A";
-  else return "B";
+  return new Rating(voyage, history).value;
 }
-function voyageRisk(voyage) {
-  let result = 1;
-  if (voyage.length > 4) result += 2;
-  if (voyage.length > 8) result += voyage.length - 8;
-  if (["china", "east-indies"].includes(voyage.zone)) result += 4;
-  return Math.max(result, 0);
-}
-function captainHistoryRisk(voyage, history) {
-  let result = 1;
-  if (history.length < 5) result += 4;
-  result += history.filter(v => v.profit < 0).length;
-  if (voyage.zone === "china" && hasChina(history)) result -= 2;
-  return Math.max(result, 0);
-}
-function hasChina(history) {
-  return history.some(v => "china" === v.zone);
-}
-function voyageProfitFactor(voyage, history) {
-  let result = 2;
-  if (voyage.zone === "china") result += 1;
-  if (voyage.zone === "east-indies") result += 1;
-  if (voyage.zone === "china" && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) result += 1;
-    if (voyage.length > 12) result += 1;
-    if (voyage.length > 18) result -= 1;
+
+class Rating {
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
   }
-  else {
-    if (history.length > 8) result += 1;
-    if (voyage.length > 14) result -= 1;
+  get value() {
+    const vpf = this.voyageProfitFactor;
+    const vr = this.voyageRisk;
+    const chr = this.captainHistoryRisk;
+    if (vpf * 3 > (vr + chr * 2)) return "A";
+    else return "B";
   }
-  return result;
+  get voyageRisk() {
+    let result = 1;
+    if (this.voyage.length > 4) result += 2;
+    if (this.voyage.length > 8) result += this.voyage.length - 8;
+    if (["china", "east-indies"].includes(this.voyage.zone)) result += 4;
+    return Math.max(result, 0);
+  }
+  get captainHistoryRisk() {
+    let result = 1;
+    if (this.history.length < 5) result += 4;
+    result += this.history.filter(v => v.profit < 0).length;
+    if (this.voyage.zone === "china" && this.hasChinaHistory) result -= 2;
+    return Math.max(result, 0);
+  }
+  get voyageProfitFactor() {
+    let result = 2;
+
+    if (this.voyage.zone === "china") result += 1;
+    if (this.voyage.zone === "east-indies") result += 1;
+    if (this.voyage.zone === "china" && this.hasChinaHistory) {
+      result += 3;
+      if (this.history.length > 10) result += 1;
+      if (this.voyage.length > 12) result += 1;
+      if (this.voyage.length > 18) result -= 1;
+    }
+    else {
+      if (this.history.length > 8) result += 1;
+      if (this.voyage.length > 14) result -= 1;
+    }
+    return result;
+  }
+  get hasChinaHistory() {
+    return this.history.some(v => "china" === v.zone);
+  }
 }
 
 // ex caller
@@ -54,3 +65,5 @@ const history = [
 ];
 
 const myRating = rating(voyage, history);
+
+// NOTE: the conditionals of 'china'
