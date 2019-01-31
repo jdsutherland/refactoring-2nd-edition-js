e4061fa9dbdb8ace9f0749605aa4fffc5f79469f
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:56:30 +0700

    Finish moving and delete class

--- ch07_encapsulation/inline_class.js
+++ ch07_encapsulation/inline_class.js
@@ -1,24 +1,17 @@
-class TrackingInformation {
-  get shippingCompany() { return this._shippingCompany; }
-  set shippingCompany(arg) { this._shippingCompany = arg; }
-  get trackingNumber() { return this._trackingNumber; }
-  set trackingNumber(arg) { this._trackingNumber = arg; }
-  get display() {
-    return `${this.shippingCompany} : $ { this.trackingNumber }
-    `;
-  }
-}
-
-// TrackingInfo seems better inlined into Shipment
-
 class Shipment {
-  get trackingInfo() { return this._trackingInformation.display; }
+  get trackingInfo() {
+    return `${this.shippingCompany} : ${this.trackingNumber}`;
+  }
   get trackingInformation() { return this._trackingInformation; }
   set trackingInformation(aTrackingInformation) {
     this._trackingInformation = aTrackingInformation;
   }
 
-  set shippingCompany(arg) { this._trackingInformation.shippingCompany = arg; }
+  get trackingNumber() { return this._trackingNumber; }
+  set trackingNumber(arg) { this._trackingNumber = arg; }
+
+  get shippingCompany() { return this._shippingCompany; }
+  set shippingCompany(arg) { this._shippingCompany = arg; }
 }
 
 // ex client
