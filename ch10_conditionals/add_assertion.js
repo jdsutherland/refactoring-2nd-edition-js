30809cca68fced80f0b90ed306c60a7acedeec2d
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 16:00:31 +0700

    Move assertion to setter

--- ch10_conditionals/add_assertion.js
+++ ch10_conditionals/add_assertion.js
@@ -1,8 +1,12 @@
 class Customer {
+  set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0);
+    this._discountRate = aNumber;
+  }
+
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
     else {
-      assert(this.discountRate >= 0);
       return aNumber - (this.discountRate * aNumber);
     }
   }
