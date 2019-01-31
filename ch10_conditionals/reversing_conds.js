1d8f4e0e4a10583286aa7a94d2cfbaa384a5bd4c
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:52:07 +0700

    step 1

--- ch10_conditionals/reversing_conds.js
+++ ch10_conditionals/reversing_conds.js
@@ -1,8 +1,7 @@
 function adjustedCapital(anInstrument) {
   let result = 0;
   if (anInstrument.capital <= 0) return result;
-  if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-  }
+  if (!(anInstrument.interestRate > 0 && anInstrument.duration > 0)) return result;
+  result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
   return result;
 }
