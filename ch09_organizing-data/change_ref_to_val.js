3e7f575e9aea7432c5b4df66ab0f3db1abecdad6
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:25:37 +0700

    Make immutatable

--- ch09_organizing-data/change_ref_to_val.js
+++ ch09_organizing-data/change_ref_to_val.js
@@ -3,14 +3,25 @@ class Person {
 
   get officeAreaCode() { return this._telephoneNumber.areaCode; }
   set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
+  set officeAreaCode(arg) {
+    this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber)
+  }
   get officeNumber() { return this._telephoneNumber.number; }
-  set officeNumber(arg) { this._telephoneNumber.number = arg; }
+  set officeNumber(arg) {
+    this._telephoneNumber = new TelephoneNumber(this.officeAreaCode, arg)
+  }
 }
 
-class TelephoneNumer {
+class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
   get areaCode()    {return this._areaCode;}
   set areaCode(arg) {this._areaCode = arg;}
 
   get number()    {return this._number;}
   set number(arg) {this._number = arg;}
 }
+
+// 1. Make TelephoneNumber immutatable
