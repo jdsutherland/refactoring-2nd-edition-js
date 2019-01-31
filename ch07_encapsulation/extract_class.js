f4d81b39615208fd4fec0d60d325b57adb3a2284
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:44:12 +0700

    Move methods as sensible

--- ch07_encapsulation/extract_class.js
+++ ch07_encapsulation/extract_class.js
@@ -1,14 +1,25 @@
 class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
+
   get name() { return this._name; }
   set name(arg) { this._name = arg; }
-  get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
-  }
-  get officeAreaCode() { return this._officeAreaCode; }
-  set officeAreaCode(arg) { this._officeAreaCode = arg; }
-  get officeNumber() { return this._officeNumber; }
-  set officeNumber(arg) { this._officeNumber = arg; }
+  get telephoneNumber() { return this._telephoneNumber.telephoneNumber; }
+  get officeAreaCode() { return this._telephoneNumber.areaCode; }
+  set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
+  get officeNumber() { return this._telephoneNumber.number; }
+  set officeNumber(arg) { this._telephoneNumber.number = arg; }
 }
 
 class TelephoneNumber {
+  get telephoneNumber() {
+    return `(${this.areaCode}) ${this.number}`;
+  }
+
+  get areaCode() { return this._areaCode; }
+  set areaCode(arg) { this._areaCode = arg; }
+
+  get number() { return this._number; }
+  set number(arg) { this._number = arg; }
 }
