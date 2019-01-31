05305245b439f8403cb3192e3d477c824a67ddd4
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 13:31:21 +0700

    Add init

---
+++ ch08_moving-features/slide_statements.js
@@ -0,0 +1,9 @@
+let result;
+if (availableResources.length === 0) {
+  result = createResource();
+  allocatedResources.push(result);
+} else {
+  result = availableResources.pop();
+  allocatedResources.push(result);
+}
+return result;
