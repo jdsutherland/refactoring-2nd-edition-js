9e1e42d9f23834c2fc04fdfac33f1f554f75a23c
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:40:47 +0700

    Extract if block

--- ch10_conditionals/decompose_conditional.js
+++ ch10_conditionals/decompose_conditional.js
@@ -1,4 +1,8 @@
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+if (summer())
   charge = quantity * plan.summerRate;
 else
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
+
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
