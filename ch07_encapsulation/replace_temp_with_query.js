bdc89e959d13e518523433461190f8b46116d98a
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 10:30:10 +0700

    Extract discountFactor

--- ch07_encapsulation/replace_temp_with_query.js
+++ ch07_encapsulation/replace_temp_with_query.js
@@ -4,13 +4,16 @@ class Order {
     this._item = item;
   }
   get price() {
-    var discountFactor = 0.98;
-    if (this.basePrice > 1000)
-      discountFactor ­ = 0.03;
-    return this.basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 
   get basePrice() {
     return this._quantity * this._item.price;
+  }
+
+  get discountFactor() {
+    let discountFactor = 0.98;
+    if (this.basePrice > 1000)
+      this.discountFactor -= 0.03;
   }
 }
