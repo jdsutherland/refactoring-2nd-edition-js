411193c43ea3f0b4d7b920b8832bdfaa06fa4082
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:47:57 +0700

    next

--- ch10_conditionals/repl_nested_conds_w_guards.js
+++ ch10_conditionals/repl_nested_conds_w_guards.js
@@ -1,18 +1,13 @@
 function payAmount(employee) {
   let result;
-  if(employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
-  else {
-    if (employee.isRetired) {
-      result = {amount: 0, reasonCode: "RET"};
-    }
-    else {
-      // logic to compute amount
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-    }
-  }
+  if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
+  if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
+
+  // logic to compute amount
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  ut.enim.ad(minim.veniam);
+  result = someFinalComputation();
   return result;
 }
