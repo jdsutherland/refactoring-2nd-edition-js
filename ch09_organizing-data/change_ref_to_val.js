8a0e525e3f9941c91df526ad2d6897e49d769784
Jeff Sutherland <jeffdsuth@gmail.com>
2019-01-31 14:28:20 +0700

    Fin

--- ch09_organizing-data/change_ref_to_val.js
+++ ch09_organizing-data/change_ref_to_val.js
@@ -22,6 +22,19 @@ class TelephoneNumber {
 
   get number()    {return this._number;}
   set number(arg) {this._number = arg;}
+
+  // js has no builtin support for repl a ref-based equality w/ value based one
+  equals(other) {
+    if (!(other instanceof TelephoneNumber)) return false;
+    return this.areaCode === other.areaCode &&
+      this.number === other.number;
+  }
 }
 
 // 1. Make TelephoneNumber immutatable
+// 2. Make complete Value Object by adding value based equality
+// (be sure to test)
+it('telephone equals', function() {
+  assert(        new TelephoneNumber("312", "555-0142")
+         .equals(new TelephoneNumber("312", "555-0142")));
+});
