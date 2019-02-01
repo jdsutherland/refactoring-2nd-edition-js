e95f685ba6108eac8e5a89e46bf87b69f7986432
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 11:28:39 +0700

    Replace tmp fn name w real

--- ch11_apis/replace_query_w_param.js
+++ ch11_apis/replace_query_w_param.js
@@ -8,11 +8,7 @@
 
 // Example:
 class HeatingPlan {
-  get targetTemperature() {
-    return xxNEWtargetTemperature(thermostat.selectedTemperature)
-  }
-
-  xxNEWtargetTemperature(selectedTemperature) {
+  targetTemperature(selectedTemperature) {
     if      (selectedTemperature >  this._max) return this._max;
     else if (selectedTemperature <  this._min) return this._min;
     else return selectedTemperature;
@@ -21,7 +17,7 @@ class HeatingPlan {
 
 
 // caller
-if      (thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature) setToHeat();
+if      (thePlan.targetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature) setToHeat();
 else if (thePlan.targetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature) setToCool();
 else setOff();
 
