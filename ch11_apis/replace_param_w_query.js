0a3bcad29c1919b4470ad8cfbb52cbd486bdf8ed
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 10:54:13 +0700

    repl temp w query

--- ch11_apis/replace_param_w_query.js
+++ ch11_apis/replace_param_w_query.js
@@ -21,10 +21,7 @@ function availableVacation(anEmployee) {
 class Order {
   get finalPrice() {
     const basePrice = this.quantity * this.itemPrice;
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
+    return this.discountedPrice(basePrice, this.discountLevel);
   }
 
   discountedPrice(basePrice, discountLevel) {
@@ -32,5 +29,9 @@ class Order {
       case 1: return basePrice * 0.95;
       case 2: return basePrice * 0.9;
     }
+  }
+
+  get discountLevel() {
+    return (this.quantity > 100) ? 2 : 1;
   }
 }
