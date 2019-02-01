e737745bbd84d341624ae3222acf5fc4ac45bd66
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 13:02:57 +0700

    Remove the dead code

--- ch11_apis/repl_cmd_w_fn.js
+++ ch11_apis/repl_cmd_w_fn.js
@@ -1,19 +1,9 @@
 // Most of the time, I just want to invoke a function and have it do its thing.
   // If that’s the case, and the function isn’t too complex, then a command object is more trouble than its worth and should be turned into a regular function.
 
-
-class ChargeCalculator {
-  constructor (customer, usage, provider){
-  }
-  get charge(customer, usage, provider) {
-    const baseCharge = customer.baseRate * usage;
-    return baseCharge + provider.connectionCharge;
-  }
-}
-
 function charge(customer, usage, provider) {
-  return new ChargeCalculator(customer, usage, provider)
-                      .charge(customer, usage, provider);
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
 
 monthCharge = charge(customer, usage, provider);
