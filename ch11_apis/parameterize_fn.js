c60963b6394c7e0fe68c8442c39361b463b5ddd9
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 10:22:25 +0700

    Replace w more universalizable

--- ch11_apis/parameterize_fn.js
+++ ch11_apis/parameterize_fn.js
@@ -1,6 +1,3 @@
-function tenPercentRaise(aPerson) {
-  aPerson.salary = aPerson.salary.multiply(1.1);
-}
-function fivePercentRaise(aPerson) {
-  aPerson.salary = aPerson.salary.multiply(1.05);
+function raise(aPerson, factor) {
+  aPerson.salary = aPerson.salary.multiply(1 + factor);
 }
