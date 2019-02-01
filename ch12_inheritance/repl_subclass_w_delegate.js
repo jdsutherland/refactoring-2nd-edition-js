0e20e9f88d4a6525d6a2a5b2da510e74b3c82d03
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 14:44:13 +0700

    Add delegate class

--- ch12_inheritance/repl_subclass_w_delegate.js
+++ ch12_inheritance/repl_subclass_w_delegate.js
@@ -11,6 +11,9 @@ class Booking {
     if (this.isPeakDay) result += Math.round(result * 0.15);
     return result;
   }
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
 }
 
 class PremiumBooking extends Booking {
@@ -30,11 +33,20 @@ class PremiumBooking extends Booking {
   }
 }
 
+class PremiumBookingDelegate {
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
+    this._extras = extras;
+  }
+}
+
 function createBooking(show, date) {
   return new Booking(show, date);
 }
 function createPremiumBooking(show, date, extras) {
-  return new PremiumBooking (show, date, extras);
+  const result = new PremiumBooking (show, date, extras);
+  result._bePremium(extras);
+  return result;
 }
 
 aBooking1 = createBooking(show,date);
