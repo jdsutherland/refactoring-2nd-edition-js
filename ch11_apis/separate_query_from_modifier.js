a5602c46ef0c6b4c969364206618a2de8c904247
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 10:19:30 +0700

    Separate query function

--- ch11_apis/separate_query_from_modifier.js
+++ ch11_apis/separate_query_from_modifier.js
@@ -12,5 +12,21 @@ function alertForMiscreant (people) {
   return "";
 }
 
+function findMiscreant (people) {
+  for (const p of people) {
+    if (p === "Don") {
+      return "Don";
+    }
+    if (p === "John") {
+      return "John";
+    }
+  }
+  return "";
+}
+
+// 1. copy the function, name after query aspect
+// 2. remove side effects
+
 // clients
-const found = alertForMiscreant(people);
+const found = findMiscreant(people);
+alertForMiscreant(people);
