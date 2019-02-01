f10e3b695aa25f98d66461576f658f727ef0e91a
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 10:55:25 +0700

    Using new query, can remove param

--- ch11_apis/replace_param_w_query.js
+++ ch11_apis/replace_param_w_query.js
@@ -21,11 +21,11 @@ function availableVacation(anEmployee) {
 class Order {
   get finalPrice() {
     const basePrice = this.quantity * this.itemPrice;
-    return this.discountedPrice(basePrice, this.discountLevel);
+    return this.discountedPrice(basePrice);
   }
 
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
+  discountedPrice(basePrice) {
+    switch (this.discountLevel) {
       case 1: return basePrice * 0.95;
       case 2: return basePrice * 0.9;
     }
