97c16176c2bdf6b3bc39a5c8817772c6ed1165f5
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 11:25:36 +0700

    inline var

--- ch11_apis/replace_query_w_param.js
+++ ch11_apis/replace_query_w_param.js
@@ -9,12 +9,16 @@
 // Example:
 class HeatingPlan {
   get targetTemperature() {
-    const selectedTemperature = thermostat.selectedTemperature;
+    return xxNEWtargetTemperature(thermostat.selectedTemperature)
+  }
+
+  xxNEWtargetTemperature(selectedTemperature) {
     if      (selectedTemperature >  this._max) return this._max;
     else if (selectedTemperature <  this._min) return this._min;
     else return selectedTemperature;
   }
 }
+
 
 // caller
 if      (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
