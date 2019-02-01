a4e22a416f6a10618ab56606820e1d2f66f6a5f9
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 10:32:34 +0700

    Extract explicit fns, removing flag argument path

--- ch11_apis/remove_flag_arg.js
+++ ch11_apis/remove_flag_arg.js
@@ -23,22 +23,24 @@ premiumBookConcert(aCustomer);
   // Also, the implementation function must be using the argument to influence its control flow, not as data that it passes to further functions.
 
 // new ex
-aShipment.deliveryDate = deliveryDate(anOrder, true);
-aShipment.deliveryDate = deliveryDate(anOrder, false);
+aShipment.deliveryDate = rushDeliveryDate(anOrder);
+aShipment.deliveryDate = regularDeliveryDate(anOrder);
 
 function deliveryDate(anOrder, isRush) {
-  if (isRush) {
+  if (isRush) return rushDeliveryDate(anOrder);
+  else        return regularDeliveryDate(anOrder);
+}
+function rushDeliveryDate(anOrder) {
     let deliveryTime;
     if (["MA", "CT"]     .includes(anOrder.deliveryState)) deliveryTime = 1;
     else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
     else deliveryTime = 3;
     return anOrder.placedOn.plusDays(1 + deliveryTime);
-  }
-  else {
+}
+function regularDeliveryDate(anOrder) {
     let deliveryTime;
     if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
     else if (["ME", "NH"] .includes(anOrder.deliveryState)) deliveryTime = 3;
     else deliveryTime = 4;
     return anOrder.placedOn.plusDays(2 + deliveryTime);
-  }
 }
