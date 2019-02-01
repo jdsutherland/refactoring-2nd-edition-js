6f9d873c628d867ebb70f6920a974e61abb2d8e2
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 12:00:08 +0700

    Remove setter - set in ctor

--- ch11_apis/remove_setting_method.js
+++ ch11_apis/remove_setting_method.js
@@ -13,17 +13,18 @@
 
 // Example:
 class Person {
+  constructor(id) {
+    this._id = id;
+  }
+
   get name()    {return this._name;}
   set name(arg) {this._name = arg;}
   get id()    {return this._id;}
-  set id(arg) {this._id = arg;}
 }
 
-const martin = new Person();
+const martin = new Person("1234");
 martin.name = "martin";
-martin.id = "1234";
 // The name of a person may change after it’s created, but the ID does not.
 // To make this clear, I want to remove the setting method for ID.
 
 // I still need to set the ID initially, so I’ll use Change Function Declaration to add it to the constructor.
-
