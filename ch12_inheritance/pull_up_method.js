64a76288013a6e0d55d57d32255a65b832ec8fcb
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 13:08:29 +0700

    init

---
+++ ch12_inheritance/pull_up_method.js
@@ -0,0 +1,14 @@
+class Employee extends Party {
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department extends Party {
+  get totalAnnualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+// I look at both classes and see that they refer to the monthlyCost property which isn’t defined on the superclass, but is present in both subclasses.
+// Since I’m in a dynamic language, I’m OK; if I were in a static language, I’d need to define an abstract method on Party.
