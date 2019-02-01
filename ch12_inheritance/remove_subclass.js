8f6ab84deb69bb302a3b838b63342296f00d630d
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 14:12:09 +0700

    Fix lack of symmetry

--- ch12_inheritance/remove_subclass.js
+++ ch12_inheritance/remove_subclass.js
@@ -4,7 +4,7 @@
 class Person {
   constructor(name, genderCode) {
     this._name = name;
-    this._genderCode = genderCode || "X";
+    this._genderCode = genderCode
   }
   get name()    {return this._name;}
   get genderCode() {return this._genderCode;}
@@ -27,7 +27,7 @@ function createPerson(aRecord) {
   switch (aRecord.gender) {
     case 'M': return new Person(aRecord.name, "M");
     case 'F': return new Person(aRecord.name, "F");
-    default:  return new Person(aRecord.name);
+    default:  return new Person(aRecord.name, "X");
   }
 }
 
