94bb275da0c36cf25a22ea051c97d8114dc4802d
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 10:14:16 +0700

    Init

---
+++ ch11_apis/separate_query_from_modifier.js
@@ -0,0 +1,16 @@
+function alertForMiscreant (people) {
+  for (const p of people) {
+    if (p === "Don") {
+      setOffAlarms();
+      return "Don";
+    }
+    if (p === "John") {
+      setOffAlarms();
+      return "John";
+    }
+  }
+  return "";
+}
+
+// clients
+const found = alertForMiscreant(people);
