52f181800eafd289b7678945b23cdcc356cf5601
Jeff Sutherland <jeffdsuth@gmail.com>
2019-02-01 10:31:03 +0700

    new ex init

--- ch11_apis/remove_flag_arg.js
+++ ch11_apis/remove_flag_arg.js
@@ -21,3 +21,24 @@ premiumBookConcert(aCustomer);
 // Not all arguments like this are flag arguments.
   // To be a flag argument, the callers must be setting the boolean value to a literal value, not data that’s flowing through the program.
   // Also, the implementation function must be using the argument to influence its control flow, not as data that it passes to further functions.
+
+// new ex
+aShipment.deliveryDate = deliveryDate(anOrder, true);
+aShipment.deliveryDate = deliveryDate(anOrder, false);
+
+function deliveryDate(anOrder, isRush) {
+  if (isRush) {
+    let deliveryTime;
+    if (["MA", "CT"]     .includes(anOrder.deliveryState)) deliveryTime = 1;
+    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else deliveryTime = 3;
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+  }
+  else {
+    let deliveryTime;
+    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (["ME", "NH"] .includes(anOrder.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
+  }
+}
